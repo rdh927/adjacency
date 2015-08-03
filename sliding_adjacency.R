f646c6ae729c9712cee733598fddc768c75fca16
mc1r_variant <rachel.dory.harris@gmail.com>
2015-08-03 17:30:05 -0500

    Update sliding_adjacency.R

--- sliding_adjacency.R
+++ sliding_adjacency.R
@@ -13,8 +13,7 @@ par(mar = c(5.1, 5.1, 4.1, 2.1))
 
 # plot using manipulate()
 manipulate(
-  plot(x,100*dbinom(x, size=6, prob = z),pch=19, ylim = c(0, 80), 
+  plot(x,100*dbinom(x, size=6, prob = z), pch = 19, ylim = c(0, 80),
        main = "Frequency of k with Changing PTC Surface Coverage", 
-       xlab = "Adjacency Number, k", ylab = "Frequency", 
-       cex = 2, cex.main = 2, cex.lab = 2, cex.axis = 2, , 
-       z = slider(0.05, 0.95))
+       xlab = "Adjacency Number, k", ylab = "Frequency", cex = 3, 
+       cex.main = 2, cex.lab = 2, cex.axis = 2), z = slider(0.05, 0.95))
